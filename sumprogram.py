a0a044b5f503e15bf1bcf27652019a154bdd306c
Roshankumawat03 <roshankumawat529@gmail.com>
2025-06-13 14:54:28 +0530

    Add files via upload

---
+++ sumprogram.py
@@ -0,0 +1,76 @@
+a=4
+b=2
+  
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+ 
+print(a+b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a-b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a*b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a/b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a//b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a%b)
+
+a=4
+b=2
+
+a=input("Enter A Number: ")
+b=input("Enter Another number: ")
+
+a=int(a)
+b=int(b)
+
+print(a**b)
