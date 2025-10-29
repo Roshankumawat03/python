088231844c689b3f50c9469847e079a48430b732
Roshankumawat03 <roshankumawat529@gmail.com>
2025-10-29 12:39:47 +0530

    Fibonacci sequence...

---
+++ fibonac_sequence.py
@@ -0,0 +1,31 @@
+# 8. Write a Python Program to Print the Fibonacci sequence.
+# Fibonacci sequence:
+# The Fibonacci sequence is a series of numbers where each number is the sum of the two
+# preceding ones, typically starting with 0 and 1. So, the sequence begins with 0 and 1, and
+# the next number is obtained by adding the previous two numbers. This pattern continues
+# indefinitely, generating a sequence that looks like this:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on.
+# Mathematically, the Fibonacci sequence can be defined using the following recurrence
+# relation:
+# 𝐹(0) = 0 𝐹(1) = 1 𝐹(𝑛) = 𝐹(𝑛 − 1) + 𝐹(𝑛 − 2)𝑓𝑜𝑟𝑛 > 1
+
+# Output Should Look Like this:
+# How many terms? 10
+# Fibonacci sequence:
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+# 13
+# 21
+# 34
+
+Terms = int(input("How many terms:"))
+print("Fibonacci sequence:")
+a,b = 0,1
+for i in range(Terms):
+    print(a)
+    a,b = b,a+b
