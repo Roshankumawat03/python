cf5a92806e4a4375d5e5bb057f5121389d3f5305
Roshankumawat03 <roshankumawat529@gmail.com>
2025-10-29 11:43:38 +0530

    random number generator ...

---
+++ random_num.py
@@ -0,0 +1,9 @@
+# Write a Python program to generate a random number. ( Hint user python random module )
+
+# Output Should Look Like this:
+# Random number: 89
+
+import random
+number = random.randint(1, 100)
+
+print("Random number:", number)
