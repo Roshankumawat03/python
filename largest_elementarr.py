394b4f1bf6865f9de0cf378e3918b945aaee37ee
Roshankumawat03 <roshankumawat529@gmail.com>
2025-10-29 13:08:45 +0530

    largest element in the array...

---
+++ largest_elementarr.py
@@ -0,0 +1,9 @@
+# Write a Python Program to find largest element in an array.
+
+# Output Should Look Like this:
+# my_array = [10, 20, 30, 99]
+# The largest element in the array is: 99
+
+my_array = [10, 97, 20, 89, 75, 30,]
+largest = max(my_array)
+print("The largest element in the array is:", largest)
