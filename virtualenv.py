7ce9ec6179a52b91f9c3efcf0377cd997d6510a9
Roshankumawat03 <roshankumawat529@gmail.com>
2025-11-07 14:50:42 +0530

    Delete virtualenv.py

--- virtualenv.py
+++
@@ -1,7 +0,0 @@
-# A Virtual Environment is a self-contained directory tree that contains a specific Python interpreter and copies of all the packages,
-# libraries, and scripts required by a particular project.
-
-
-# pip -> python package manager
-
-# python -m venv env_name
